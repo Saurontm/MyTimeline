0cd8a450b55abc367e8ea8d11b1695fbeee7798e
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-10 16:16:40 +0300

    removed timeline in  Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import "react-vertical-timeline-component/style.min.css";
-import TimeLine from "./TimeLine";
+
 import { Title, Subtitle } from "../styles";
 
 const HomePage = () => {
@@ -7,7 +7,6 @@ const HomePage = () => {
     <div>
       <Title>Sarah's life</Title>
       <Subtitle>A timeline of some of Sarah's favorite years</Subtitle>
-      <TimeLine />
     </div>
   );
 };
